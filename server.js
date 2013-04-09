23776462587dd435d55ba9ed8bc5226130501903
defrainfox <daniel@sclbits.com>
2013-04-09 14:59:48 -0500

    add files

---
+++ server.js
@@ -0,0 +1,12 @@
+var myserver = require('socket.io').listen(8080);
+
+myserver.sockets.on('connection',arranque);
+
+function arranque(usuario){
+	usuario.on('nuevoDato',emitir);
+}
+
+function emitir(data){
+	myserver.sockets.emit('mensajeServidor',data);
+	console.log(data);
+}
